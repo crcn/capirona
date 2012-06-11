77374d545a298f8fc83b78bb79263d209cb0e037
Craig Condon <craig@spiceapps.com>
2012-06-11 00:00:45 -0500

    ability to load directory of scripts

--- examples/load/index.js
+++ examples/load/index.js
@@ -3,8 +3,13 @@ var capirona = require("../../");
 
 capirona.make({
 	"def loadTest": {
-		"run": {
-			"load": __dirname + "/tasks"
-		}
+		"run": [
+			{
+				"load": __dirname + "/tasks"
+			},
+			{
+				"load": __dirname + "/dir"
+			}
+		]
 	}
 }, "loadTest");
